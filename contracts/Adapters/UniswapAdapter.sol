d0ae0d761cd3d9f76c3da4fcc9db118a336274c3
Emily Williams <emily.williams@levelk.io>
2019-06-18 14:28:33 -0400

    Make UniswapAdapter compatible with decimals (by using biiig numbers)

--- contracts/Adapters/UniswapAdapter.sol
+++ contracts/Adapters/UniswapAdapter.sol
@@ -8,6 +8,8 @@ import "@aragon/os/contracts/lib/math/SafeMath.sol";
 contract UniswapAdapter is IAdapter {
   using SafeMath for uint;
 
+  event PriceCalculated(uint price, address token1, address token2);
+
   UniswapFactoryInterface uniswapFactory;
 
   constructor(address _uniswapFactory) {
@@ -21,6 +23,8 @@ contract UniswapAdapter is IAdapter {
     uint256 token1PriceEth = token1Exchange.getTokenToEthInputPrice(1 ether);
     uint256 token2PriceEth = token2Exchange.getTokenToEthInputPrice(1 ether);
 
-    return token1PriceEth.div(token2PriceEth);
+    // calculate price in wei (price * 10 ** 18)
+    price = (token1PriceEth.mul(1 ether)).div(token2PriceEth);
+    emit PriceCalculated(price, token1, token2);
   }
 }
