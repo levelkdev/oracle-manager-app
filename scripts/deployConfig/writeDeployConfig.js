010527f30d912421acf36bed499c004b003a7fed
Emily Williams <emily.williams@levelk.io>
2019-07-01 14:35:21 -0400

    Take out writeDeployConfig functionality

--- scripts/deployConfig/writeDeployConfig.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-
-module.exports = function writeDeployConfig (network, deployConfig) {
-  fs.writeFileSync(
-    `deploy.${network}.json`,
-    JSON.stringify(deployConfig, null, 4)
-  )
-}
--- scripts/utilities/tryDeployToNetwork.js
+++ scripts/utilities/tryDeployToNetwork.js
@@ -1,6 +1,5 @@
 const configForNetwork = require('../deployConfig/configForNetwork')
 const isLocalNetwork = require('../deployConfig/isLocalNetwork')
-const writeDeployConfig = require('../deployConfig/writeDeployConfig')
 
 const tryDeployToNetwork = async (network, contractArtifact, contractName, params = []) => {
   const deployConfig = configForNetwork(network)
@@ -10,10 +9,6 @@ const tryDeployToNetwork = async (network, contractArtifact, contractName, param
     console.log(`Deploying ${contractName}...`)
     contractInstance = await contractArtifact.new.apply(null, params)
     console.log(`Deployed ${contractName}: ${contractInstance.address}`)
-    if (!isLocalNetwork(network)) {
-      deployConfig[contractName] = contractInstance.address
-      writeDeployConfig(network, deployConfig)
-    }
   } else {
     contractInstance = await contractArtifact.at(deployedAddress)
     console.log(`${contractName} already deployed: ${deployedAddress}`)
