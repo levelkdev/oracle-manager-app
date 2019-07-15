8e7325caa5d7de755f07efe451841f3b8253265d
Mike Calvanese <mfcalvanese@gmail.com>
2019-07-15 16:41:46 -0400

    fix bug in aragonRedux
    The first event from the Aragon app was `undefined`. This handles that case

--- app/aragonRedux/aragonRedux.test.js
+++ app/aragonRedux/aragonRedux.test.js
@@ -1,18 +1,57 @@
 import assert from 'assert'
-import { differenceWithEvents, pascalToUpperSnake } from './aragonRedux'
+import { filterNewEvents, pascalToUpperSnake } from './aragonRedux'
 
 const mockEvent1 = { event: 'MOCK_EVENT', transactionHash: 'a1b2c3' }
 const mockEvent2 = { event: 'MOCK_EVENT', transactionHash: 'd4e5f6' }
+const mockEventNoTxHash = { event: 'MOCK_EVENT' }
 
-const mockEventState1 = [ mockEvent1 ]
-const mockEventState2 = [ mockEvent1, mockEvent2 ]
+const mockEventStateInitial = [ mockEvent1 ]
+const mockEventStateFinal = [ mockEvent1, mockEvent2 ]
+const mockEventStateWithUndefined = [ undefined, mockEvent1 ]
+const mockEventStateWithoutTxHash = [ mockEventNoTxHash, mockEvent1 ]
 
 describe('aragonRedux', () => {
-  describe('differenceWithEvents()', () => {
+  describe('filterNewEvents()', () => {
     describe('when given different initial and final event states', () => {
       it('should return an array of the new events', () => {
         assert.deepEqual(
-          differenceWithEvents(mockEventState1, mockEventState2),
+          filterNewEvents(mockEventStateInitial, mockEventStateFinal),
+          [ mockEvent2 ]
+        )
+      })
+    })
+
+    describe('when final event state array has an undefined value', () => {
+      it('should return an array of the new events without the undefined value', () => {
+        assert.deepEqual(
+          filterNewEvents([], mockEventStateWithUndefined),
+          [ mockEvent1 ]
+        )
+      })
+    })
+
+    describe('when initial event state array has an undefined value', () => {
+      it('should return an array of the new events without the undefined value', () => {
+        assert.deepEqual(
+          filterNewEvents(mockEventStateWithUndefined, mockEventStateFinal),
+          [ mockEvent2 ]
+        )
+      })
+    })
+
+    describe('when final event state array has an event with no transaction hash', () => {
+      it('should return an array of the new events without that value', () => {
+        assert.deepEqual(
+          filterNewEvents([], mockEventStateWithoutTxHash),
+          [ mockEvent1 ]
+        )
+      })
+    })
+
+    describe('when initial event state array has an event with no transaction hash', () => {
+      it('should return an array of the new events without that value', () => {
+        assert.deepEqual(
+          filterNewEvents(mockEventStateWithoutTxHash, mockEventStateFinal),
           [ mockEvent2 ]
         )
       })
@@ -21,7 +60,7 @@ describe('aragonRedux', () => {
     describe('when given the same initial and final event states', () => {
       it('should return an empty array', () => {
         assert.deepEqual(
-          differenceWithEvents(mockEventState2, mockEventState2),
+          filterNewEvents(mockEventStateFinal, mockEventStateFinal),
           [ ]
         )
       })
