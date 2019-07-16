65fde4fd7dc7df767be2c60e6608bae5b4a192bc
Mike Calvanese <mfcalvanese@gmail.com>
2019-07-16 14:17:28 -0400

    use IdentityBadge for address display

--- app/components/OracleList.js
+++ app/components/OracleList.js
@@ -1,5 +1,11 @@
 import React from 'react'
-import { Table, TableHeader, TableRow, TableCell, Text } from '@aragon/ui'
+import {
+  IdentityBadge,
+  Table,
+  TableHeader,
+  TableRow,
+  TableCell
+} from '@aragon/ui'
 
 const OracleList = ({ oracles }) => (
   <Table
@@ -12,7 +18,7 @@ const OracleList = ({ oracles }) => (
     {oracles.map(({ address }) => (
       <TableRow key={address}>
         <TableCell>
-          <Text>{address}</Text>
+          <IdentityBadge entity={address} />
         </TableCell>
       </TableRow>
     ))}
