ce47248dc80b4d419a48517a0bbfe0d25f0fc946
Emily Williams <emily.williams@levelk.io>
2019-09-06 13:17:27 -0400

    IDataFeed: take out setReult()

--- contracts/IDataFeed.sol
+++ contracts/IDataFeed.sol
@@ -3,7 +3,6 @@ pragma solidity >=0.4.24;
 import "tidbit/contracts/DataFeedOracles/DataFeedOracleBase.sol";
 
 interface IDataFeed {
-  function setResult(DataFeedOracleBase[] memory _dataFeeds) public;
   function logResult() public;
   function addDataFeed(DataFeedOracleBase dataFeed) public;
   function removeDataFeed(DataFeedOracleBase dataFeed) public;
